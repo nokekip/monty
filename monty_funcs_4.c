6b4bf05a6ec709e6da5e9fb6d22bbe3a4f7fc231
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-10-09 21:19:01 +0300

    56

--- monty_funcs_4.c
+++ monty_funcs_4.c
@@ -1,80 +1,107 @@
 #include "monty.h"
 
-void monty_rotl(stack_t **stack, unsigned int line_number);
-void monty_rotr(stack_t **stack, unsigned int line_number);
-void monty_stack(stack_t **stack, unsigned int line_number);
-void monty_queue(stack_t **stack, unsigned int line_number);
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ *
+ * @head: the head of the doubly linked list
+ * @n: the value of the node to add
+ *
+ * Return: the new pointer
+ */
+
+stack_t *add_dnodeint(stack_t **head, const int n)
+{
+	stack_t *new = NULL;
+
+	new = malloc(sizeof(stack_t));
+	if (new)
+	{
+		new->n = n;
+		new->prev = NULL;
+		if (*head)
+			(*head)->prev = new;
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		META.error = 1;
+	}
+
+	return (new);
+}
 
 /**
- * monty_rotl - Rotates the top value of a stack_t linked list to the bottom.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ *
+ * @head: the head of the doubly linked list
+ * @n: the value of the node to add
+ *
+ * Return: the new pointer
  */
-void monty_rotl(stack_t **stack, unsigned int line_number)
-{
-	stack_t *top, *bottom;
 
-	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+stack_t *add_dnodeint_end(stack_t **head, const int n)
+{
+	stack_t *new = NULL;
+	stack_t *cur = *head;
+
+	new = malloc(sizeof(stack_t));
+	if (new)
+	{
+		new->n = n;
+		new->next = NULL;
+		new->prev = NULL;
+
+		if (!(*head))
+			*head = new;
+		else
+		{
+			while (cur->next)
+				cur = cur->next;
+			cur->next = new;
+			new->prev = cur;
+		}
+	}
+	else
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		META.error = 1;
+	}
+
+	return (new);
+}
+
+/**
+ * _push - adds a value at top (if in STACK mode)
+ * or bottom (if in QUEUE mode) of stack
+ *
+ * @stack: the program stack
+ * @line_nb: the line number
+ * @a_value: value to add to stack
+ */
+
+void _push(stack_t **stack, unsigned int line_nb, char *a_value)
+{
+	size_t i = 0;
+
+	while (a_value && a_value[i])
+	{
+		if (isdigit(a_value[i]) || (a_value[i] == '-' && strlen(a_value) > 1))
+			i++;
+		else
+			break;
+	}
+
+	if (!a_value || a_value == '\0' || i != strlen(a_value))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_nb);
+		META.error = 1;
 		return;
+	}
 
-	top = (*stack)->next;
-	bottom = (*stack)->next;
-	while (bottom->next != NULL)
-		bottom = bottom->next;
-
-	top->next->prev = *stack;
-	(*stack)->next = top->next;
-	bottom->next = top;
-	top->next = NULL;
-	top->prev = bottom;
-
-	(void)line_number;
-}
-
-/**
- * monty_rotr - Rotates the bottom value of a stack_t linked list to the top.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
- */
-void monty_rotr(stack_t **stack, unsigned int line_number)
-{
-	stack_t *top, *bottom;
-
-	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
-		return;
-
-	top = (*stack)->next;
-	bottom = (*stack)->next;
-	while (bottom->next != NULL)
-		bottom = bottom->next;
-
-	bottom->prev->next = NULL;
-	(*stack)->next = bottom;
-	bottom->prev = *stack;
-	bottom->next = top;
-	top->prev = bottom;
-
-	(void)line_number;
-}
-
-/**
- * monty_stack - Converts a queue to a stack.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
- */
-void monty_stack(stack_t **stack, unsigned int line_number)
-{
-	(*stack)->n = STACK;
-	(void)line_number;
-}
-
-/**
- * monty_queue - Converts a stack to a queue.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
- */
-void monty_queue(stack_t **stack, unsigned int line_number)
-{
-	(*stack)->n = QUEUE;
-	(void)line_number;
+	if (META.state == STACK)
+		add_dnodeint(stack, atoi(a_value));
+	else
+		add_dnodeint_end(stack, atoi(a_value));
 }
